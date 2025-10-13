021a94896972db200db5d5dc6807547eaeb6567f
David Morales <lxdavidxl639@gmail.com>
2025-10-13 19:38:06 -0400

    staticfiles_storage

--- church/settings.py
+++ church/settings.py
@@ -95,7 +95,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Use Whitenoise for production static files
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # ------------------------------------------------------------
 # SECURITY HEADERS (Recommended for Production)
