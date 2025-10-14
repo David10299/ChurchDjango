26719bbce2d81153cb0c3d7e310ab4e146f76b4c
David Morales <lxdavidxl639@gmail.com>
2025-10-14 00:10:28 -0400

    attempt to fix css

--- church/settings.py
+++ church/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 # ------------------------------------------------------------
 SECRET_KEY = config('SECRET_KEY', default='change-this-secret')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
 # ------------------------------------------------------------
